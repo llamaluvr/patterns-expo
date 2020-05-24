050614763a4ec041a2062284ac9470cbb5a2c1a1
Keith Kurak <keith.kurak@gmail.com>
2020-05-24 14:23:52 -0400

    roughed in location detection

--- components/events/EventDetail.js
+++ components/events/EventDetail.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
+import { DateTime } from 'luxon';
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useThemeContext } from "../../config/ThemeContext";
+
+function IconButton({ icon, text, onPress }) {
+  const { sizes, colors, textStyles } = useThemeContext();
+  return (
+    <View style={{ flex: 1, }}>
+      <TouchableWithoutFeedback onPress={onPress} >
+        <View style={{ alignItems: "center", justifyContent: "flex-start" }}>
+          {icon}
+          <Text style={[textStyles.standard.dark, { textAlign: 'center'}]}>{text}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
 
 function EventDetail({ events, tag, selectedEvent }) {
   const { sizes, colors, textStyles } = useThemeContext();
 
+  console.log(selectedEvent)
+
   return (
     <View style={{ flex: 1, backgroundColor: colors.bg0 }}>
-      <View style={{ alignItems: "center", justifyContent: "center", marginTop: sizes.large }}>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: sizes.large,
+        }}
+      >
         <View
           style={{
             height: 80,
@@ -24,9 +48,35 @@ function EventDetail({ events, tag, selectedEvent }) {
             {tag.name[0] + tag.name[1]}
           </Text>
         </View>
-        <Text
-          style={[textStyles.standard.dark, { marginTop: sizes.medium }]}
-        >{tag.name}</Text>
+        <Text style={[textStyles.standard.dark, { marginTop: sizes.medium }]}>
+          {tag.name}
+        </Text>
+        <View
+          style={{
+            marginTop: sizes.large,
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <IconButton
+            icon={
+              <AntDesign name="calendar" size={40} color={colors.darkText} />
+            }
+            onPress={() => {}}
+            text={DateTime.fromJSDate(selectedEvent.date).toFormat('MMM d, yyyy\nh:mm a')}
+          />
+          <IconButton
+            icon={
+              <MaterialCommunityIcons
+                name="map-marker"
+                size={40}
+                color={colors.darkText}
+              />
+            }
+            onPress={() => {}}
+            text={selectedEvent.address ? selectedEvent.address[0].name + '\n' + selectedEvent.address[0].city : '???'}
+          />
+        </View>
       </View>
     </View>
   );
--- storyboard/StoryboardDataWrapper.js
+++ storyboard/StoryboardDataWrapper.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react";
+import * as Location from "expo-location";
 import Storyboard from "./Storyboard";
 import ThemeProvider from "../config/ThemeContext";
 
@@ -76,12 +77,46 @@ const defaultTags = [
   },
 ];
 
+// adds async functionality to any 
+function dispatchMiddleware(dispatch) {
+  return async (action) => {
+    switch (action.type) {
+      case "addEvent":
+        // execute actual dispatch to create new event
+        dispatch(action);
+        // execute updating location after initial create
+        let { status } = await Location.requestPermissionsAsync();
+        let location = null;
+        if (status !== "granted") {
+          console.log("Location permission denied!");
+        } else {
+          location = await Location.getCurrentPositionAsync({});
+          let address = await Location.reverseGeocodeAsync(location.coords);
+          const newAction = { type: 'updateEvent', payload: { id: action.payload.id, location, address }};
+          dispatch(newAction);
+        }
+        break;
+      default:
+        return dispatch(action);
+    }
+  };
+}
+
+// only includes actual modifications to state
 function reducer(state, action) {
   switch (action.type) {
     case "addEvent":
       return { ...state, events: state.events.concat([action.payload]) };
     case "addTag":
       return { ...state, tags: state.tags.concat([action.payload]) };
+    case "updateEvent":
+      return {
+        ...state,
+        events: state.events.map((e) =>
+          // acts like a patch
+          e.id === action.payload.id ? { ...e, ...action.payload } : e
+        ),
+      };
     default:
       throw new Error();
   }
@@ -95,7 +130,7 @@ function StoryboardDataWrapper() {
 
   return (
     <ThemeProvider>
-      <Storyboard {...state} dispatch={dispatch} />
+      <Storyboard {...state} dispatch={dispatchMiddleware(dispatch)} />
     </ThemeProvider>
   );
 }
